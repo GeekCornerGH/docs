46eaf8d0201b41327cd921a9cfc0c23b6714d731
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-12-05 12:31:55 +0100

    fix: document not defined

---
+++ src/pages/lastvideo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function LatestVideo() {
+	fetchVideo()
+	return (
+		
+		<Layout title="Dernière vidéo" description="Retrouvez ici la dernière vidéo de GeekCorner">
+			<h2 style={{  padding: "10% 0",textAlign: "center"}}>Vous serez redirigé(e) vers la dernière vidéo de GeekCorner...</h2>
+		</Layout >
+	)
+}
+async function fetchVideo() {
+        if (typeof window !== "undefined") {
+	        const baseUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D';
+	        const data=await fetch(baseUrl + "UCEn1rLR7XssIfQaS5M4xj0Q");
+	        const res = await data.json()
+	        if (!res) return history.pushState(null, "", "/");
+	        else document.location.href=res.items[0].link;
+        }
+	
+}
