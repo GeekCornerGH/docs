f431dbc7bf9574942e06fd0eede2e91ff1658b9e
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2023-02-03 16:56:01 +0100

    fix: Hide useless stuff

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,7 +141,7 @@ const config = {
             activeBaseRegex: "docs\/|helios\/"
 
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          /*{ to: '/blog', label: 'Blog', position: 'left' },*/ //Uncomment to show blog
           {
             type: 'localeDropdown',
             position: 'right',
