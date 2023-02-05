280932950437024b20fff2a224b88921b0f94a72
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2023-02-05 19:40:24 +0100

    feat: Translate home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Translate, {translate} from '@docusaurus/Translate';
 
 import styles from './index.module.css';
 
@@ -13,7 +14,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <Translate id="homepage.subtitle"><p className="hero__subtitle">Bienvenue! Vous trouverez sur ce site la documentation relative aux tutos sur ma chaîne YouTube.</p></Translate>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
