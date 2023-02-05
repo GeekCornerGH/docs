1c235c7c0044fb3f1ec6de6e7dce819e45716bbe
ItsJeBoyGoogle <73650787+sjempotje@users.noreply.github.com>
2023-02-05 17:46:01 +0100

    Some translations

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'fr',
-    locales: ['fr', 'en', 'nl'],
+    locales: ['fr', 'en', 'nl', 'ru'],
   },
   plugins: [
     [
